f5cf635c782b54da5ae60f974237ae888699c138
Alexey <decent-alex@yandex.ru>
2018-08-01 11:49:28 +0300

    Add env files
    Add env files

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const SERVER = 'http://localhost:5000';
+export const API_URL = process.env.REACT_APP_API_URL;
 
 export function getAvailableTables() {
-    return axios.get(`${SERVER}/api/tables`).then((response) => response.data);
+    return axios.get(`${API_URL}/api/tables`).then((response) => response.data);
 }
 
 export function getTable(tableName) {
-    return axios.get(`${SERVER}/api/table/${tableName}`).then((response) => response.data);
+    return axios.get(`${API_URL}/api/table/${tableName}`).then((response) => response.data);
 }
